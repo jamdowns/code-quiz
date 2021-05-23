5c9ae3cbbd1c6dfea08bfd90049298d73ca121dd
jamaur downs <jamaurdowns@jamaurs-mbp-2.home>
2021-05-23 16:03:32 -0400

    created basic layout for quiz and question data

---
+++ script.js
@@ -0,0 +1,39 @@
+const quizData = [
+    {
+        question: 'A form that ask for or gives a response of true or false would be considered a:',
+        a: 'Good or Bad',
+        b: 'True or False',
+        c: 'Online or Offline',
+        d: 'Based or Unbased',
+        correrct: "b"
+    }, {
+        question: 'A variable can take a value type of:',
+        a: 'Boolean, Integer, Phrase',
+        b: 'String, Boolean, Integer',
+        c: 'String, Yarn, Thread',
+        d: 'Integer, String, Bottle',
+        correct: 'b',
+    }, {
+        question: 'What tag is used to call on a .js file in an html file?',
+        a: '<h1>',
+        b: '<body>',
+        c: '<link>',
+        d: '<script>',
+        correct: 'd',
+    }, {
+        question: 'An array in javascript can be used to store all of the following except:',
+        a: 'Strings and arrays',
+        b: 'Booleans',
+        c: 'Integers',
+        d: 'Furniture',
+        correct: 'd',
+    }, {
+        question: 'In and if/else statement, how are the decalarations separated?',
+        a: '{}',
+        b: '[]',
+        c: '()',
+        d: '||',
+        correct: 'a',
+    },
+    
+];
