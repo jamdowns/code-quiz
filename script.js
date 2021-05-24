9a3d8632ff4d823e5a910f0975b5ea2b7e0ad363
jamaur downs <jamaurdowns@jamaurs-mbp-2.home>
2021-05-23 21:28:39 -0400

    started writing program in js file

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//establish data for questions, answers, and correct answers
 const quizData = [
     {
         question: 'A form that ask for or gives a response of true or false would be considered a:',
@@ -35,5 +36,59 @@ const quizData = [
         d: '||',
         correct: 'a',
     },
-    
+
 ];
+//establish variables to grab html positions
+var question = document.getElementById("question");
+var aText = document.getElementById("a-text");
+var bText = document.getElementById("b-text");
+var cText = document.getElementById("c-text");
+var dText = document.getElementById("d-text");
+var submit = document.getElementById("submit");
+
+let currentQuestion = 0;
+let score = 0;
+
+//function to make the quiz run
+startQuiz();
+
+function startQuiz() {
+    //change the question data to match the current question
+    //and answers based on position of quiz
+    var currentQuizData = quizData[currentQuestion];
+
+    question.innerText = currentQuizData.question;
+    aText.innerText = currentQuizData.a;
+    bText.innerText = currentQuizData.b;
+    cText.innerText = currentQuizData.c;
+    dText.innerText = currentQuizData.d;
+
+
+};
+
+//compare selected answers to correct answers to allow progression in quiz
+//allow progression with correct answer
+//wrong answer gives prompt
+//record score as time left on timer
+function itemSelected() {
+    const answerEls = document.querySelectorAll(".choice");
+
+    answerEls.forEach((answer) function () {
+        if (answerEl.checked) {
+            return answerEl.id;
+        }
+    });
+
+    return undefined;
+
+};
+
+submit.addEventListener("click", function () {
+    currentQuestion++;
+    if (currentQuestion < quizData.length) {
+        startQuiz();
+    } else {
+        //alert of score and option to save high score
+        alert("Congratulations!");
+    };
+});
